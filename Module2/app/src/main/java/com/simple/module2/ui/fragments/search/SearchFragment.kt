84b397f4f774655069a27722d28e84a9b3be6d12
DGSW_양동현 <66651059+DGSWDongHyun@users.noreply.github.com>
2021-03-26 16:18:00 +0900

    모듈 2 마무리

--- Module2/app/src/main/java/com/simple/module2/ui/fragments/search/SearchFragment.kt
+++ Module2/app/src/main/java/com/simple/module2/ui/fragments/search/SearchFragment.kt
@@ -1,11 +1,15 @@
 package com.simple.module2.ui.fragments.search
 
 import android.os.Bundle
+import android.view.ContextMenu
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.simple.module2.R
+import kotlinx.android.synthetic.main.fragment_search.*
 
 class SearchFragment : Fragment() {
 
@@ -13,5 +17,19 @@ class SearchFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val searchView = view.findViewById<SearchView>(R.id.searchView)
+        val recentView = view.findViewById<ConstraintLayout>(R.id.recentSearchView)
+
+        searchView.setOnFocusChangeListener { v, hasFocus ->
+            if(hasFocus)
+                recentView.visibility = View.VISIBLE
+            else
+                recentView.visibility = View.GONE
+        }
     }
 }
